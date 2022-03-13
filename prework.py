cccb801223a3d977332766ab34a09ee38d10568f
Jose Anthony Llanes <janthony022@gmail.com>
2022-03-12 18:01:35 -0800

    addition

---
+++ prework.py
@@ -0,0 +1,9 @@
+print ("Hello coding temple!")
+
+number_mil = list(range(1,1000001))
+for numbers in number_mil:
+    print(numbers)
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
